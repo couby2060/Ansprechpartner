d47b1ecf525d3bd7d642256b778f0c4e23532a89
johannes.wilhelm <johannes.wilhelm@twt-rb.de>
2020-04-08 17:36:40 +0200

    Pfad zur CSV hinzugefügt

--- ap.py
+++ ap.py
@@ -14,7 +14,7 @@ def main():
 	projektname = []
 	ansprechpartner = []
 	#READ CSV File
-	with open('currentprojects.csv') as csvDataFile:
+	with open('/Users/jwilhelm/Desktop/jwi-test/py/Ansprechpartner/currentprojects.csv') as csvDataFile:
 		csvReader = csv.reader(csvDataFile, delimiter=';', quotechar='"')
 		for row in csvReader:
 			#print (row)
